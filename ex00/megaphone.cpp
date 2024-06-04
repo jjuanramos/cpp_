78b6d4549fbcc61400b819d476e2cbbe7767fd26
jjuanramos <jjuanramos91@gmail.com>
2024-06-04 13:31:02 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,27 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 12:55:56 by juramos           #+#    #+#             */
-/*   Updated: 2024/06/04 13:11:25 by juramos          ###   ########.fr       */
+/*   Updated: 2024/06/04 13:29:59 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main (void)
+int	main (int argc, char **argv)
 {
-	std::cout << "HELLO" << std::endl;
+	int			i;
+	std::string s;
+
+	i = 0;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE NOISE *";
+	else
+		while (++i < argc)
+		{
+			s = argv[i];
+			std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+			std::cout << s;
+		}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
 }
