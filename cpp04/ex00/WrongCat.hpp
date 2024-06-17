b4f3fb4096cdcda8fdaa273d7f623378bd5f4be8
jjuanramos <jjuanramos91@gmail.com>
2024-06-17 13:10:38 +0200

    remove virtual

--- cpp04/ex00/WrongCat.hpp
+++ cpp04/ex00/WrongCat.hpp
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 13:05:32 by juramos           #+#    #+#             */
-/*   Updated: 2024/06/17 13:05:53 by juramos          ###   ########.fr       */
+/*   Updated: 2024/06/17 13:10:19 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 class WrongCat: public WrongAnimal
 {
 	public:
-						WrongCat();
-						~WrongCat();
-		virtual void	makeSound() const;
+				WrongCat();
+				~WrongCat();
+		void	makeSound() const;
 };
 
 #endif
