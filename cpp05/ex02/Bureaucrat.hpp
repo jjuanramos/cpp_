2da1e46a459528b1c2a8edc09f38d1575aa0ea65
jjuanramos <jjuanramos91@gmail.com>
2024-06-28 12:31:09 +0200

    first commit ex02

---
+++ cpp05/ex02/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 10:30:21 by juramos           #+#    #+#             */
+/*   Updated: 2024/06/28 11:46:28 by juramos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <iostream>
+#include <stdexcept>
+#include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+private:
+	std::string const	_name;
+	int					_range;
+public:
+	Bureaucrat(std::string name, int range);
+	~Bureaucrat();
+	std::string	getName() const;
+	int			getRange() const;
+	void		signForm(Form& f, std::string reason);
+	Bureaucrat&	operator++();
+	Bureaucrat&	operator--();
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		virtual const char* what() const throw();
+	};
+};
+
+std::ostream&	operator<<(std::ostream& o, Bureaucrat const& b);
+
+#endif
---
+++ cpp05/ex02/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 10:30:04 by juramos           #+#    #+#             */
+/*   Updated: 2024/06/28 11:40:05 by juramos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+int	main()
+{
+	try
+	{
+		Bureaucrat	Mike("Mike", 3);
+		Form		Stupid("Stupid", 5, 10);
+		Form		Coolio("Coolio", 1, 20);
+
+		Stupid.beSigned(Mike);
+		try
+		{
+			Coolio.beSigned(Mike);
+		}
+		catch (Form::GradeTooLowException& e)
+		{
+			Mike.signForm(Coolio, e.what());
+		}
+	}
+	catch (Bureaucrat::GradeTooHighException& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Unexpected exception." << std::endl;
+	}
+	return (0);
+}
