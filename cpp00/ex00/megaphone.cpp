2ffca33d24757214df7c914af8ae1c5798db032b
juramos <juramos@student.42madrid.com>
2024-07-05 11:12:10 +0200

    Fixed issue with transform

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,11 +6,12 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 12:55:56 by juramos           #+#    #+#             */
-/*   Updated: 2024/07/05 11:02:20 by juramos          ###   ########.fr       */
+/*   Updated: 2024/07/05 11:11:37 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
 int	main (int argc, char **argv)
 {
@@ -24,7 +25,9 @@ int	main (int argc, char **argv)
 		while (++i < (size_t)argc)
 		{
 			s = argv[i];
-			std::transform(s.begin(), s.end(), s.begin(), ::toupper);
+			std::string::size_type	j = -1;
+			while (++j < s.length())
+				s[j] = std::toupper(s[j]);
 			std::cout << s;
 		}
 	std::cout << std::endl;
