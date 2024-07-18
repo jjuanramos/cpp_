56dd988484e78d97d1961004ef1b5f043d0f15fe
juramos <juramos@student.42madrid.com>
2024-07-18 12:41:42 +0200

    updated claptrap

--- cpp03/ex02/ClapTrap.hpp
+++ cpp03/ex02/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 10:14:05 by juramos           #+#    #+#             */
-/*   Updated: 2024/06/17 11:57:49 by juramos          ###   ########.fr       */
+/*   Updated: 2024/07/18 12:40:55 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,24 @@
 class ClapTrap
 {
 private:
-	std::string	_name;
-	int			_hit_pts;
-	int			_energy_pts;
-	int			_atk_dmg;
+	std::string		_name;
+	int				_hit_pts;
+	int				_energy_pts;
+	int				_atk_dmg;
+					ClapTrap();
 public:
-				ClapTrap(std::string name, int _hit_pts, int _energy_pts, int _atk_dmg);
-				ClapTrap(ClapTrap const& copy);
-				~ClapTrap();
-	ClapTrap&	operator=(ClapTrap const& other);
-	void		attack(const std::string& target);
-	std::string	getName() const;
-	void		takeDamage(unsigned int amount);
-	void		beRepaired(unsigned int amount);
+					ClapTrap(std::string name, int _hit_pts, int _energy_pts, int _atk_dmg);
+					ClapTrap(ClapTrap const& copy);
+					~ClapTrap();
+	ClapTrap&		operator=(ClapTrap const& other);
+	virtual void	attack(const std::string& target);
+	std::string		getName() const;
+	int				getHitPts() const;
+	int				getEnergyPts() const;
+	int				getAtkDmg() const;
+	void			setEnergyPts(int val);
+	void			takeDamage(unsigned int amount);
+	void			beRepaired(unsigned int amount);
 };
 
 #endif
