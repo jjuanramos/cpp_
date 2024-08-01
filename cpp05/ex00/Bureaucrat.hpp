9944d70116c06d48016c7522bd15526a13ca032d
juramos <juramos@student.42madrid.com>
2024-08-01 11:19:11 +0200

    Added orthodox canonical class

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: juramos <juramos@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 10:30:21 by juramos           #+#    #+#             */
-/*   Updated: 2024/06/27 10:52:40 by juramos          ###   ########.fr       */
+/*   Updated: 2024/08/01 11:15:36 by juramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ private:
 	std::string const	_name;
 	int					_range;
 public:
-	Bureaucrat(std::string name, int range);
-	~Bureaucrat();
+				Bureaucrat(std::string name, int range);
+				Bureaucrat(Bureaucrat const& copy);
+	Bureaucrat&	operator=(Bureaucrat const& other);
+				~Bureaucrat();
 	std::string	getName() const;
 	int			getRange() const;
 	Bureaucrat&	operator++();
